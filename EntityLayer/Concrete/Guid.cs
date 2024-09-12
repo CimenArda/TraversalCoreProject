77e05ae972477e183d2e9dc73128042630d5d8da
ArdaCimen <cimenarda.yaz@gmail.com>
2024-09-12 15:43:41 +0300

    Entitylerin Tamamlanması

---
+++ EntityLayer/Concrete/Guid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Guid
+    {
+
+        public int GuidID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string Link1 { get; set; }
+        public string Link2 { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
