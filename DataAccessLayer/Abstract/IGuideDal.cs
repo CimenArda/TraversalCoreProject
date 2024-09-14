990793aa56ce2b67c72a1b294b26b4812a8500c6
ArdaCimen <cimenarda.yaz@gmail.com>
2024-09-14 12:26:11 +0300

    Data Access Layer İşlemlerin Tamamlanması

---
+++ DataAccessLayer/Abstract/IGuideDal.cs
@@ -0,0 +1,14 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Guid = EntityLayer.Concrete.Guid;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGuideDal:IGenericDal<Guid>
+    {
+    }
+}
---
+++ DataAccessLayer/Repository/GenericRepository.cs
@@ -0,0 +1,51 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repository
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        
+        public List<T> GetAllList()
+        {
+            using var c = new Context();
+           return c.Set<T>().ToList();
+        }
+
+        public T GetById(T entity)
+        {
+            using var c = new Context();
+
+            return c.Set<T>().Find(entity);
+        }
+
+        public void Insert(T entity)
+        {
+            using var c = new Context();
+
+            c.Add(entity);
+            c.SaveChanges();
+        }
+
+        public void Remove(T entity)
+        {
+            using var c = new Context();
+
+            c.Remove(entity);
+            c.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            using var c = new Context();
+
+          c.Update(entity);
+            c.SaveChanges();
+        }
+    }
+}
