b3d88dfed2895ed801b4c5b4bc70e382608f59ff
ArdaCimen <cimenarda.yaz@gmail.com>
2024-09-17 13:30:38 +0300

    SignIn/SignUp Sayfalarının Hazırlanması

---
+++ TraversalCoreProject/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Controllers
+{
+    [AllowAnonymous]
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        //public IActionResult SignUp()
+        //{
+        //    return View();
+        //}
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        //public IActionResult SignIn()
+        //{
+        //    return View();
+        //}
+
+
+
+    }
+}
