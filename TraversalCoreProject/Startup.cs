37b5253f0deea82c91255b6f80de469b33fa4862
ArdaCimen <cimenarda.yaz@gmail.com>
2024-09-18 13:28:13 +0300

    Custom Identity Validator

--- TraversalCoreProject/Startup.cs
+++ TraversalCoreProject/Startup.cs
@@ -17,6 +17,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TraversalCoreProject.Models;
 
 namespace TraversalCoreProject
 {
@@ -33,7 +34,7 @@ namespace TraversalCoreProject
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<Context>();
-            services.AddIdentity<AppUser,AppRole>().AddEntityFrameworkStores<Context>();
+            services.AddIdentity<AppUser,AppRole>().AddEntityFrameworkStores<Context>().AddErrorDescriber<CustomIdentityValidator>().AddEntityFrameworkStores<Context>();
 
             services.AddMvc(config =>
             {
