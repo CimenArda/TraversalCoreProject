79402396aea4932afbffb99d9066d1af803d9d4d
ArdaCimen <cimenarda.yaz@gmail.com>
2024-09-18 14:41:59 +0300

    Login İşleminin Gerçekleştirilmesi

--- TraversalCoreProject/Controllers/LoginController.cs
+++ TraversalCoreProject/Controllers/LoginController.cs
@@ -12,13 +12,15 @@ namespace TraversalCoreProject.Controllers
     {
 
         private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
 
-		public LoginController(UserManager<AppUser> userManager)
-		{
-			_userManager = userManager;
-		}
+        public LoginController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
 
-		[HttpGet]
+        [HttpGet]
         public IActionResult SignUp()
         {
             return View();
@@ -61,10 +63,21 @@ namespace TraversalCoreProject.Controllers
         {
             return View();
         }
-        //public IActionResult SignIn()
-        //{
-        //    return View();
-        //}
+        [HttpPost]
+        public async Task<IActionResult> SignIn(UserSignInViewModel p)
+        {
+            if (ModelState.IsValid)
+            {
+                var result =await _signInManager.PasswordSignInAsync(p.UserName,p.Password,false,true);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index","Destination");
+                }
+                return RedirectToAction("SignIn","Index");
+
+            }
+            return View();
+        }
 
 
 
---
+++ TraversalCoreProject/Models/UserSignInViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TraversalCoreProject.Models
+{
+    public class UserSignInViewModel
+    {
+        [Required(ErrorMessage ="Kullanıcı Adınızı Giriniz!")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage ="Şifre Alanı Zorunludur!")]
+        public string Password { get; set; }
+    }
+}
