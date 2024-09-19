90f96a158585d25354f3119327db96dafdff6b90
ArdaCimen <cimenarda.yaz@gmail.com>
2024-09-19 13:38:39 +0300

    Reservation Sınıfı

---
+++ BusinessLayer/Concrete/ReservationManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ReservationManager : IReservationService
+    {
+        private readonly IReservationDal _reservationDal;
+
+        public ReservationManager(IReservationDal reservationDal)
+        {
+            _reservationDal = reservationDal;
+        }
+
+        public List<Reservation> TGetAllList()
+        {
+            return _reservationDal.GetAllList();
+        }
+
+        public Reservation TGetById(int id)
+        {
+            return _reservationDal.GetById(id);
+        }
+
+        public void TInsert(Reservation entity)
+        {
+            _reservationDal.Insert(entity);
+        }
+
+        public void TRemove(Reservation entity)
+        {
+            _reservationDal.Remove(entity);
+        }
+
+        public void TUpdate(Reservation entity)
+        {
+            _reservationDal.Update(entity);
+        }
+    }
+}
