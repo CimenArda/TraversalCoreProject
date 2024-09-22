bda7b641031e5ed3c0a46d206c19911fee002000
ArdaCimen <cimenarda.yaz@gmail.com>
2024-09-22 11:46:26 +0300

    Dashboard 'Proje Platform Ayarları View Component

---
+++ TraversalCoreProject/ViewComponents/MemberDashboard/_PlatformSettings.cs
@@ -0,0 +1,13 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.MemberDashboard
+{
+    public class _PlatformSettings :ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
