58fd5287df95b6e487ab4a11966eaf0b48a24292
ArdaCimen <cimenarda.yaz@gmail.com>
2024-09-23 15:19:47 +0300

    Hata Sayfası,Rehber Listesi Ve Rehber Ekleme

---
+++ TraversalCoreProject/Areas/Admin/Controllers/GuideController.cs
@@ -0,0 +1,67 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/Guide")]
+    public class GuideController : Controller
+    {
+        private readonly IGuideService _guideService;
+
+        public GuideController(IGuideService guideService)
+        {
+            _guideService = guideService;
+        }
+
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            var values =  _guideService.TGetAllList();
+            return View(values);
+
+            
+        }
+
+        [HttpGet]
+        [Route("AddGuide")]
+
+        public IActionResult AddGuide()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("AddGuide")]
+        public IActionResult AddGuide(Guid g)
+        {
+            _guideService.TInsert(g);
+
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditGuide(int id)
+        {
+          var values =  _guideService.TGetById(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult EditGuide(Guid g)
+        {
+            _guideService.TUpdate(g);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult ChangeToTrue(int id)
+        {
+            return RedirectToAction("Index");
+
+        }
+        public IActionResult ChangeToFalse(int id)
+        {
+            return RedirectToAction("Index");
+
+        }
+    }
+}
