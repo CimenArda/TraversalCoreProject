165cbe662d3eb928fde20dacea70c68b1ebccbf3
ArdaCimen <cimenarda.yaz@gmail.com>
2024-10-06 13:53:29 +0300

    Tasarımsal Düzenlemeler - 1

--- TraversalCoreProject/Controllers/DestinationController.cs
+++ TraversalCoreProject/Controllers/DestinationController.cs
@@ -1,9 +1,11 @@
 ﻿using BusinessLayer.Abstract;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProject.Controllers
 {
+    [AllowAnonymous]
     public class DestinationController : Controller
     {
         private readonly IDestinationService _destinationService;
