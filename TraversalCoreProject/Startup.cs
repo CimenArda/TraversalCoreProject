d3482742a4c8436c4bf85593300ce3c7454d4ce8
ArdaCimen <cimenarda.yaz@gmail.com>
2024-10-12 15:13:55 +0300

    Member Kısmında Yeni Template Ve Düzenlemeler -2 Çoklu Dil Desteği

--- TraversalCoreProject/Startup.cs
+++ TraversalCoreProject/Startup.cs
@@ -15,6 +15,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -71,6 +72,13 @@ namespace TraversalCoreProject
 
             services.AddMvc();
 
+            services.AddLocalization(opt =>
+            {
+                opt.ResourcesPath = "Resources";
+            });
+
+            services.AddMvc().AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix).AddDataAnnotationsLocalization();
+
             services.ConfigureApplicationCookie(opt =>
             {
                 opt.LoginPath = "/Login/SignIn";
@@ -115,6 +123,11 @@ namespace TraversalCoreProject
             app.UseRouting();
 
             app.UseAuthorization();
+
+            var suppertedCultures = new[] { "en", "fr", "es", "gr", "tr", "de" };
+            var localizationOptions = new RequestLocalizationOptions().SetDefaultCulture(suppertedCultures[1]).AddSupportedCultures(suppertedCultures).AddSupportedUICultures(suppertedCultures);
+            app.UseRequestLocalization(localizationOptions);
+
 
             app.UseEndpoints(endpoints =>
             {
